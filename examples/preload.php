e9aad376960d7a27091fbb1cac49497114612850
Iskander Sharipov <quasilyte@gmail.com>
2022-02-18 16:47:14 +0300

    add preload.php for PHP compatibility

---
+++ examples/preload.php
@@ -0,0 +1,12 @@
+<?php
+
+// This preload script is needed for PHP.
+//
+// Use something like this to enable preloading:
+// php -d opcache.enable_cli=1 -d opcache.preload ./examples/preload.php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use KSQLite3\KSQLite3;
+
+KSQLite3::loadFFI();
