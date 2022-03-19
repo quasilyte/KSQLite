c2df0d0346c045bab3b08e21453f8c3cecb59b5d
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-03-19 22:20:37 +0300

    fix typo in a comment

--- preload.php
+++ preload.php
@@ -3,7 +3,7 @@
 // This preload script is needed for PHP.
 //
 // Use something like this to enable preloading:
-// php -d opcache.enable_cli=1 -d opcache.preload preload.php
+// php -d opcache.enable_cli=1 -d opcache.preload=preload.php
 
 require_once __DIR__ . '/vendor/autoload.php';
 
